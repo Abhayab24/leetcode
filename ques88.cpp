e0c84cf53e04b63b6ed710a61fdf1d8106c2160a
Kumar Abhay Partap Singh <abhaypartap542005@gmail.com>
2025-08-04 23:27:05 +0530

    mergiing sorted array using tpa(two pointer approach)

---
+++ ques88.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1; 
+
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
+
+int main() {
+    vector<int> nums1 = {1, 3, 5, 0, 0, 0};
+    vector<int> nums2 = {2, 4, 6};
+    int m = 3, n = 3;
+
+    merge(nums1, m, nums2, n);
+
+    cout << "Merged array: ";
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
