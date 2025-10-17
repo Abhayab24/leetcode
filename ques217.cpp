e8e1d60d44625c12e7babe474e48228eaee4fd93
Kumar Abhay Partap Singh <abhaypartap542005@gmail.com>
2025-10-17 21:37:05 +0530

    containing duplicate

---
+++ ques217.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        int size = nums.size();
+
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < size; i++){
+            if(nums[i] == nums[i-1]) return true;
+        }
+        return false;
+    }
+};
+int main(){
+    Solution sol;
+    vector<int> nums = {1,2,3,1};
+    bool res = sol.containsDuplicate(nums);
+    cout << (res ? "true" : "false") << endl;
+    return 0;
+}
