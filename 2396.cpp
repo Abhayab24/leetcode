26bf705a509b0315383bced69d843fb4e5d8c44c
Kumar Abhay Partap Singh <abhaypartap542005@gmail.com>
2025-10-28 08:48:48 +0530

    stictly palindromic number

---
+++ 2396.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
+int main() {
+    Solution sol;
+    int n = 4;
+    cout << boolalpha << sol.isStrictlyPalindromic(n) << endl; // Output: false
+    return 0;
+}
