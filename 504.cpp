7023f1db75e32b8dc0567a9f70372e62ab031912
Abhay Rana <abhaypartap542005@gmail.com>
2025-11-06 13:43:34 +0530

    convertToBase7

---
+++ 504.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+class solution{
+string converToBase7(int num){
+    if(num<0) return "0";
+    bool neg=num<0;#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if (num == 0) return "0";  // base case for 0
+        bool neg = num < 0;
+        if (neg) num = -num;
+
+        string res = "";
+        while (num > 0) {
+            int rem = num % 7;
+            res = to_string(rem) + res;
+            num /= 7;
+        }
+
+        if (neg) res = "-" + res;
+        return res;
+    }
+};
+
+int main() {
+    int num;
+    cin >> num;
+
+    Solution s;
+    cout << s.convertToBase7(num);
+}
+
+    if(neg) num=-num;
+    string res="";
+    while(num>0){
+        int rem=num%7;
+        res=to_string(rem)+res;
+        num/=7;
+    }
+    if(neg) res="-"+res;
+    return res;
+
+}
+];
+int main()
+{
+    int num;
+    cin>>num;
+    cout<<converToBase7(202);
+}
