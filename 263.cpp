4f02df3b5d3634f21309adb15e5e664b475dec7f
Abhay Rana <abhaypartap542005@gmail.com>
2025-11-12 22:08:47 +0530

    Ugly Number-But solved with Brute force

---
+++ 263.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int maxDivide(int a ,int b){
+        while(a%b==0){
+            a/=b;
+        }
+        return a;
+    }
+    bool isUgly(int n){
+        if(n<=0) return false;
+        n=maxDivide(n,2);
+        n=maxDivide(n,3);
+        n=maxDivide(n,5);
+        return (n==1)?1:0;
+    }
+};
+int main(){
+    Solution s;
+    int n;
+    cin>>n;
+    cout<<s.isUgly(n);
+    return 0;
+}
