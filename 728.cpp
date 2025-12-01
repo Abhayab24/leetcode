46282b96cfa1db77a7511db6d1ee836e7f7fff2a
Abhay Rana <abhaypartap542005@gmail.com>
2025-12-01 23:05:50 +0530

    code

---
+++ 728.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+
+        for (int num = left; num <= right; num++) {
+            int temp = num;
+            bool ok = true;
+
+            while (temp > 0) {
+                int digit = temp % 10;
+                
+                if (digit == 0 || num % digit != 0) {
+                    ok = false;
+                    break;
+                }
+                temp = temp / 10;
+            }
+
+            if (ok) {
+                ans.push_back(num);
+            }
+        }
+
+        return ans;
+    }
+};
