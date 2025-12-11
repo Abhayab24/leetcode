8388d9934f5a98e74ad6b3b904ce3f4338e9163e
Abhay Rana <abhaypartap542005@gmail.com>
2025-12-11 22:23:10 +0530

    Solved with Brute Force Approach

---
+++ 169.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            int freq=1;
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    freq++;
+                }
+            }
+            if(freq>n/2){
+                return nums[i];
+            }
+        }
+       return -1;
+    }
+   
+};
+int main(){
+    Solution s; 
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    vector<int>nums(n);
+    cout<<"Enter elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    int answer=s.majorityElement(nums);
+    cout<<"Majority Element is: "<<answer;
+    
+}
