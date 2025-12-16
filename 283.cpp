b508c0559d3d39542d21557c3235fd6edc6bb6a8
Abhay Rana <abhaypartap542005@gmail.com>
2025-12-16 21:06:50 +0530

    move all zeroes to end

---
+++ 283.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+        int nums[5]={0,1,0,3,12};
+        int j=0;
+        for(int i=0;i<5;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+        for(int i=0;i<5;i++){
+            cout<<nums[i]<<" ";
+        }
+    }
