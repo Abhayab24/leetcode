a663b379c3f963d906ef887bbc8816ee1b2c63ba
Abhay Rana <abhaypartap542005@gmail.com>
2026-02-11 16:08:58 +0530

    set mismatch

---
+++ 645.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> nums ={1,2,2,4};
+        unordered_map<int,int>mp;
+        vector<int> store;
+        for(int x:nums){
+            mp[x]++;
+        }
+        int missing=0;
+        int duplicate=0;
+        for(int i=0;i<nums.size();i++){
+            if(mp[i+1]<1){
+               missing=i+1;
+            }
+            if(mp[i+1]>1){
+               duplicate=i+1;
+                
+            }
+        }
+        
+        store.push_back(duplicate);
+        store.push_back(missing);
+        for(int x:store){
+            cout<<x<<" ";
+        }
+    }
