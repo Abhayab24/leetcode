2fdf57e3321c205ecedc8bbcdd5fd049f9e21d59
Abhay Rana <152707398+Abhayab24@users.noreply.github.com>
2026-03-27 22:41:22 +0530

    Time: 0 ms (100%), Space: 27.8 MB (41.04%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n=nums.size();
+        int i=1;
+        if(nums[i]<=nums[i-1]) return false;
+        if(n<4) return false;
+        while(i<n && nums[i]>nums[i-1]){
+            i++;
+        }
+        if(i==n ||nums[i]>=nums[i-1]) return false;
+        while(i<n && nums[i]<nums[i-1]){
+            i++;
+        }
+        if(i==n || nums[i]<nums[i-1])return false;
+        while(i<n&&nums[i]>nums[i-1]){
+        i++;
+        }
+        
+    return i==n;
+    }
+};
