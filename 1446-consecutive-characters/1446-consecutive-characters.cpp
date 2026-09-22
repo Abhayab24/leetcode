c4d08b164ac3319bbe4c1f6bb925243ff63345ba
Abhay Rana <152707398+Abhayab24@users.noreply.github.com>
2026-09-22 12:45:05 +0530

    Time: 0 ms (100%), Space: 9.7 MB (14.59%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxPower(string s) {
+        int c=1;
+        int m=1;
+        for(int i=1;i<s.length();i++){
+            if(s[i]==s[i-1]){
+                c++;
+            }
+            else{
+                c=1;
+            }
+            if(c>m){
+                m=c;
+            }
+        }
+        return m;
+    }
+};
